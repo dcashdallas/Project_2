0cc3f2ec6419c33cbea2ec88425bd2bcf14f4e8e
dcashdallas <dcashdallas81@yahoo.com>
2020-07-02 19:43:09 -0500

    Deploy for Heroku

--- server.js
+++ server.js
@@ -20,6 +20,10 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.get("/", function (req, res) {
+  res.json(path.join(__dirname, "public/index.html"));
+});
+
 // Requiring our routes
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
